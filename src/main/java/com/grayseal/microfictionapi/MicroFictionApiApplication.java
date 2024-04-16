da756c0980b51f2cc6f291fdc2fbcf1fcc7b6b43
lynnemunini <muninilynne65@gmail.com>
2024-04-16 17:43:12 +0300

    Set up project and dependencies

---
+++ src/main/java/com/grayseal/microfictionapi/MicroFictionApiApplication.java
@@ -0,0 +1,13 @@
+package com.grayseal.microfictionapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroFictionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroFictionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grayseal/microfictionapi/MicrofictionapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grayseal.microfictionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrofictionapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
