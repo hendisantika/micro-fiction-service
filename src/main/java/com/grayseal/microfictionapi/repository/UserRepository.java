95cfbb63502ee1bdb56f230f8e86e3aa012f9482
lynnemunini <muninilynne65@gmail.com>
2024-04-24 18:44:58 +0300

    Set up mysql dependencies and set the credentials as environment variables

---
+++ src/main/java/com/grayseal/microfictionapi/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.grayseal.microfictionapi.repository;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+
+import com.grayseal.microfictionapi.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
